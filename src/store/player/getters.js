afb47e425b516df324b124ed81a5f5b7e6dcc9a4
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-12-02 00:00:21 +0900

    :sparkles: (store/player) add getters of victory or defeat judger

--- src/store/player/getters.js
+++ src/store/player/getters.js
@@ -1,5 +1,24 @@
 export default {
   name(state) {
     return state.player
+  },
+
+  turnPlayer(state, getters, rootState, rootGetters) {
+    return rootGetters['game/turnPlayer']
+  },
+
+  isYourTurn(state, getters) {
+    return getters.name === getters.turnPlayer
+  },
+
+  victoryOrDefeat(state, getters, rootState, rootGetters) {
+    switch (rootGetters['game/winner']) {
+      case getters.name:
+        return 'WIN'
+      case 'DRAW':
+        return 'DRAW'
+      default:
+        return 'LOSE'
+    }
   }
 }
