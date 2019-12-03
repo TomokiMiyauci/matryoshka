4369890839151565d6e325fdf8c86f0b59ab6de2
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-12-03 23:38:07 +0900

    :recycle: (layouts/default) difine main playrooms page as default

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,16 +1,32 @@
 <template>
   <v-app>
-    <v-navigation-drawer app right> </v-navigation-drawer>
-    <v-app-bar app></v-app-bar>
+    <v-app-bar app> </v-app-bar>
     <v-content>
       <nuxt />
     </v-content>
+    <v-bottom-navigation v-model="btmNav" app fixed>
+      <v-btn to="/playrooms" value="playrooms">
+        <span>Playrooms</span>
+        <v-icon>mdi-playstation</v-icon>
+      </v-btn>
+
+      <v-btn to="/share" value="share">
+        <span>Share</span>
+        <v-icon>mdi-share-variant</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
   </v-app>
 </template>
 
 <script>
 export default {
-  components: {}
+  components: {},
+
+  data() {
+    return {
+      btmNav: null
+    }
+  }
 }
 </script>
 
