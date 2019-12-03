563a0d00880b0402c03ffe4002f2c2118dbc2f31
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-12-04 00:25:10 +0900

    :sparkles: (img/qrcode) add site qrcode image

---
+++ src/pages/share/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container class="fill-height">
+    <v-row justify="center">
+      <v-img :src="require('~/assets/img/qrcode.png')" max-width="200px" />
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
