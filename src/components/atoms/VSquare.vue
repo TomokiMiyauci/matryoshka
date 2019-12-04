7bd1f22bf99992c218cf9c2df6809161d1abd2df
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-12-04 23:25:05 +0900

    :lipstick: (atoms/VSuare) change background-color

--- src/components/atoms/VSquare.vue
+++ src/components/atoms/VSquare.vue
@@ -1,5 +1,9 @@
 <template>
-  <td @click="onClick" :class="{ placeable: isValidClass }" class="square">
+  <td
+    @click="onClick"
+    :class="{ placeable: isValidClass, bounce: value }"
+    class="square"
+  >
     {{ value }}
   </td>
 </template>
@@ -35,27 +39,58 @@ export default {
   max-width: 100px;
   max-height: 100px;
   font-size: 60px;
-  border: 1px solid #ccc;
+  border: 1px solid rgba(128, 128, 128, 0.781);
   text-align: center;
+  background-color: rgb(80, 80, 80);
 }
 
 @keyframes active {
   0% {
-    background: red;
+    background: rgba(88, 21, 212, 0.226);
   }
   100% {
-    background: blue;
+    background: rgba(88, 21, 212, 0.671);
+    border: 2px solid rgba(241, 226, 15, 0.767);
+    box-shadow: 1px 1px 8px 5px rgba(255, 255, 0, 0.8) inset;
   }
 }
 .placeable {
   animation-name: active;
-  animation-duration: 2s;
-  animation-timing-function: linear;
+  animation-duration: 1.5s;
+  animation-timing-function: ease;
   animation-iteration-count: infinite;
   animation-direction: alternate;
   cursor: pointer;
 }
 .placeable:hover {
   opacity: 0.5;
+}
+.bounce {
+  animation-name: bounce;
+  animation-duration: 1s;
+  animation-timing-function: ease;
+  animation-iteration-count: 1;
+}
+@keyframes bounce {
+  0% {
+    transform: translateY(0);
+    background: yellow;
+  }
+
+  30% {
+    transform: translateY(-100px);
+  }
+
+  50% {
+    transform: translateY(0px);
+  }
+
+  60% {
+    transform: translateY(-30px);
+  }
+
+  100% {
+    transform: translateY(0);
+  }
 }
 </style>
