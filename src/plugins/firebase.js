acfc61c5bf86f96dcf6e40f76037caad0fe1cfa2
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-12-08 17:53:59 +0900

    :wrench: (firebase.js) hide parameter to env

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -5,14 +5,14 @@ import 'firebase/auth'
 import 'firebase/messaging'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDNkeqmUApjilJj-R09XluRcSpDP4yR0Ss',
-  authDomain: 'vobblet-vobblers.firebaseapp.com',
-  databaseURL: 'https://vobblet-vobblers.firebaseio.com',
-  projectId: 'vobblet-vobblers',
-  storageBucket: 'vobblet-vobblers.appspot.com',
-  messagingSenderId: '216717069811',
-  appId: '1:216717069811:web:c84303e0fedc12b35a5686',
-  measurementId: 'G-BCTBVXH00B'
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 }
 
 if (!firebase.apps.length) {
