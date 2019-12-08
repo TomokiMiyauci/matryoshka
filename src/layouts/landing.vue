a85ffdc91419bd073787fbd588210070dc02bea2
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-12-08 16:27:13 +0900

    :sparkles: add layouts for landing page, playroom

---
+++ src/layouts/landing.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-content class="aa">
+      <nuxt />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.aa {
+  background: linear-gradient(#e66465, #9198e5);
+}
+</style>
---
+++ src/layouts/playroom.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <!-- <v-navigation-drawer app fixed right> </v-navigation-drawer> -->
+    <v-app-bar app>
+      <v-btn to="/playrooms" icon>
+        <v-icon>mdi-arrow-left</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <nuxt />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
