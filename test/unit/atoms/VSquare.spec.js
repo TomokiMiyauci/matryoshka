d501d1eae3ac72f1a223a406f854db59f574cbb9
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-12-17 23:42:28 +0900

    :white_check_mark: (atoms/VSquare) add atoms of VSquare basic test

--- test/unit/atoms/VSquare.spec.js
+++ test/unit/atoms/VSquare.spec.js
@@ -2,8 +2,32 @@ import { shallowMount } from '@vue/test-utils'
 import VSquare from '~/components/atoms/VSquare'
 
 describe('VSquare', () => {
-  it('should be ', () => {
+  it('should trigger onClick method when it clicked', () => {
     const wrapper = shallowMount(VSquare)
-    expect(wrapper)
+    const stub = jest.fn()
+    wrapper.setMethods({ onClick: stub })
+
+    wrapper.trigger('click')
+
+    expect(stub).toHaveBeenCalled()
+  })
+
+  it('should emit when onClick called', () => {
+    const wrapper = shallowMount(VSquare)
+
+    wrapper.trigger('click')
+
+    expect(wrapper.emitted().click).toBeTruthy()
+  })
+
+  it('should have something value between props', () => {
+    const value = 'test'
+    const wrapper = shallowMount(VSquare, {
+      propsData: {
+        value
+      }
+    })
+
+    expect(wrapper.text()).toBe(value)
   })
 })
