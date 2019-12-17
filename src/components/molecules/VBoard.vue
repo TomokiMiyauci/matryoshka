7a1b9c9cba453b3b373eca1803fb7c91d83765be
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-12-17 22:19:55 +0900

    :recycle: (molecules/VBoard) rewrite to component declare iterable

--- src/components/molecules/VBoard.vue
+++ src/components/molecules/VBoard.vue
@@ -3,55 +3,15 @@
     <v-row justify="center" align-content="center">
       <table>
         <tbody>
-          <tr>
+          <tr v-for="(rows, row) in squares" :key="row">
             <v-square
-              :value="squares[0]"
+              v-for="(cols, col) in rows"
+              :key="row + col"
+              :value="cols.value"
               :isValidClass="placeable(0)"
-              @click="click(0)"
-            ></v-square>
-            <v-square
-              :value="squares[1]"
-              :isValidClass="placeable(1)"
-              @click="click(1)"
-            ></v-square>
-            <v-square
-              :value="squares[2]"
-              :isValidClass="placeable(2)"
-              @click="click(2)"
-            ></v-square>
-          </tr>
-          <tr>
-            <v-square
-              :value="squares[3]"
-              :isValidClass="placeable(3)"
-              @click="click(3)"
-            ></v-square>
-            <v-square
-              :value="squares[4]"
-              :isValidClass="placeable(4)"
-              @click="click(4)"
-            ></v-square>
-            <v-square
-              :value="squares[5]"
-              :isValidClass="placeable(5)"
-              @click="click(5)"
-            ></v-square>
-          </tr>
-          <tr>
-            <v-square
-              :value="squares[6]"
-              :isValidClass="placeable(6)"
-              @click="click(6)"
-            ></v-square>
-            <v-square
-              :value="squares[7]"
-              :isValidClass="placeable(7)"
-              @click="click(7)"
-            ></v-square>
-            <v-square
-              :value="squares[8]"
-              :isValidClass="placeable(8)"
-              @click="click(8)"
+              @click="click({ row, col })"
+              :isValidRed="isValidRed(cols)"
+              :isValidBlue="isValidBlue(cols)"
             ></v-square>
           </tr>
         </tbody>
@@ -81,7 +41,16 @@ export default {
 
   methods: {
     click(payload) {
+      console.log(payload)
       this.$emit('click', payload)
+    },
+
+    isValidRed(val) {
+      return val.player === 'PLAYER_1'
+    },
+
+    isValidBlue(val) {
+      return val.player === 'PLAYER_2'
     }
   }
 }
