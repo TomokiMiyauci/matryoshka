8197695f1cdfd1052dc70455a2e7bb3800bda2c9
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-12-17 22:25:04 +0900

    :sparkles: (pages/playrooms) add watcher of round what trigger initialize game

--- src/pages/playrooms/_uid.vue
+++ src/pages/playrooms/_uid.vue
@@ -1,9 +1,18 @@
 <template>
   <v-container class="fill-height">
     <!-- <div class="text-center">Your Turn</div> -->
-    <v-wait-sign @up="TIME_UP()" :isYourTurn="isYourTurn"></v-wait-sign>
+    <!-- <v-wait-sign @up="TIME_UP()" :isYourTurn="isYourTurn"></v-wait-sign> -->
 
     <the-playground></the-playground>
+
+    <v-container style="padding:0;margin:0;">
+      <v-row justify="center" align="center" align-content="center">
+        <v-col v-for="piece in holdingPieces" :key="piece.id" cols="auto">
+          <v-btn @click="onCli(piece)">{{ piece.value }}</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+
     <v-dialog :value="victoryOrDefeat" persistent max-width="290">
       <v-card>
         <v-card-title class="headline">{{ title }}</v-card-title>
@@ -19,37 +28,33 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
-
+    <!--
     <v-overlay :value="!isYourTurn">
       <v-turn></v-turn>
 
-      <v-btn @click="onClickLose" fixed bottom left color="indigo"
+      <v-btn @click="onClickLose" fixed top right color="indigo"
         >Surrender
         <v-icon rigth>mdi-handshake</v-icon>
       </v-btn>
-    </v-overlay>
+    </v-overlay>  -->
   </v-container>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
-import VTurn from '~/components/atoms/VTurn'
 
-import VWaitSign from '~/components/molecules/VWaitSign'
 import ThePlayground from '~/components/organisms/ThePlayground'
 export default {
   layout: 'playroom',
 
   components: {
-    ThePlayground,
-    VWaitSign,
-    VTurn
+    ThePlayground
   },
 
   computed: {
-    ...mapGetters('game', ['turnPlayer']),
+    ...mapGetters('game', ['turnPlayer', 'holdingPieces', 'round']),
     ...mapGetters('modal', ['isOpened']),
-    ...mapGetters('player', ['isYourTurn', 'victoryOrDefeat']),
+    ...mapGetters('player', ['isYourTurn', 'victoryOrDefeat', 'name']),
 
     title() {
       switch (this.victoryOrDefeat) {
@@ -72,14 +77,31 @@ export default {
     }
   },
 
+  watch: {
+    round() {
+      this.INIT_HOLDING_PIECE(this.name)
+    }
+  },
+
   created() {
-    this.bindPlayroomRef()
-    this.bindGameRef()
+    // await this.bindPlayroomRef()
+    // await this.bindGameRef()
+  },
+
+  mounted() {
+    this.INIT_HOLDING_PIECE(this.name)
   },
 
   methods: {
     ...mapActions('modal', ['HIDE']),
-    ...mapActions('game', ['RESTART', 'NEXT_GAME', 'bindGameRef', 'TIME_UP']),
+    ...mapActions('game', [
+      'RESTART',
+      'NEXT_GAME',
+      'bindGameRef',
+      'TIME_UP',
+      'ADD_SELECTING_PIECE',
+      'INIT_HOLDING_PIECE'
+    ]),
     ...mapActions('playroom', ['bindPlayroomRef']),
     ...mapActions('player', ['SURRENDER', 'LEAVE_ROOM']),
 
@@ -96,9 +118,19 @@ export default {
 
     onClickLose() {
       this.SURRENDER()
+    },
+
+    onCli(payload) {
+      console.log(payload)
+      this.ADD_SELECTING_PIECE({ ...payload, type: 'PLACE' })
     }
   }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.cccc {
+  bottom: 0px;
+  position: absolute;
+}
+</style>
