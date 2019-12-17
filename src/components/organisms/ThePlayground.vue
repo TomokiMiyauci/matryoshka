636129b86220ae3f5b0f0cbf81a9b6abc4087413
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-12-17 22:27:12 +0900

    :sparkles: (components/organisms) add click event what judge on pieces are placed or moved

--- src/components/organisms/ThePlayground.vue
+++ src/components/organisms/ThePlayground.vue
@@ -1,6 +1,6 @@
 <template>
   <v-board
-    :squares="latestBoard"
+    :squares="deepLatestBoard"
     :placeable="placeable"
     @click="onClick"
   ></v-board>
@@ -16,23 +16,75 @@ export default {
   },
 
   computed: {
-    ...mapGetters('game', ['latestBoard', 'willBeNextBoard', 'placeable'])
+    ...mapGetters('game', [
+      'latestBoard',
+      'willBeNextBoard',
+      'placeable',
+      'selectingPiece',
+      'deepLatestBoard',
+      'isSelecting',
+      'latestPiece'
+    ])
   },
 
-  watch: {
-    latestBoard() {
-      this.ASSESS_STATUS()
-    }
+  mounted() {
+    console.log(this.deepLatestBoard)
   },
+
+  // watch: {
+  //   latestBoard() {
+  //     this.ASSESS_STATUS()
+  //   },
+  //   bb() {
+  //     alert()
+  //   }
+  // },
 
   methods: {
-    ...mapActions('player', ['ACTION']),
-    ...mapActions('game', ['ASSESS_STATUS']),
+    ...mapActions('player', ['PLACE_PIECE', 'MOVE_PIECE']),
+    ...mapActions('game', ['ASSESS_STATUS', 'ADD_SELECTING_PIECE']),
 
     onClick(payload) {
-      console.log(this.willBeNextBoard(payload))
-      this.ACTION(payload)
-      // this.addHistoryRecord(payload)
+      // TODO:  if selecting piece
+      // if click board
+      // @Place
+      // case:empty
+      // case:exist something value
+      // else
+      // @MOVE
+      // case:exist something value
+      //
+      // case:empty => return
+
+      if (this.isSelecting) {
+        switch (this.selectingPiece.type) {
+          case 'PLACE':
+            console.log('select', this.selectingPiece)
+
+            const newValue = { ...this.selectingPiece, ...payload }
+            this.PLACE_PIECE(newValue)
+            break
+          case 'MOVE':
+            const { row, col, type, ...rest } = this.selectingPiece
+            const news = {
+              value: { ...rest },
+              type,
+              from: { col, row },
+              to: { ...payload }
+            }
+            console.log(news)
+
+            this.MOVE_PIECE(news)
+
+            break
+        }
+      } else {
+        const { row, col } = payload
+        const latestPiece = this.latestPiece(row, col)
+        if (latestPiece.value) {
+          this.ADD_SELECTING_PIECE({ ...latestPiece, type: 'MOVE' })
+        }
+      }
     }
   }
 }
