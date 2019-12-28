cd87c3050d8c3990591e2a3899cde48b7ab68daa
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-12-28 21:32:49 +0900

    :art: (functions/) apply typescript suggestion

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,7 @@
-import * as functions from 'firebase-functions';
-import * as admin from "firebase-admin";
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
 
 admin.initializeApp()
-
 
 const winCombinations = [
   [
@@ -47,71 +46,85 @@ const winCombinations = [
   ]
 ]
 
-
-export const testFunc =
-  functions.firestore.document('todos/{id}').onUpdate((change, context)=>{
-    const id:string = context.params.id
-    if(id !== 'HO8cX8IFtTDOR1E7lpPb')return
+export const testFunc = functions
+  .region('asia-northeast1')
+  .firestore.document('todos/{id}')
+  .onUpdate((change, context) => {
+    const id: string = context.params.id
+    if (id !== 'HO8cX8IFtTDOR1E7lpPb') return
 
     const data = change.after.data()
     const previousData = change.before.data()
     console.log('start')
 
-    if(data === undefined || previousData === undefined || data.history.length <=2 || 'winner' in data || 'winner' in previousData)return
-    if(data.winner)return
+    if (
+      data === undefined ||
+      previousData === undefined ||
+      data.history.length <= 2 ||
+      'winner' in data ||
+      'winner' in previousData
+    )
+      return
+    if (data.winner) return
 
-    const deepLatestBoard = deepen(3,3,data.history.slice(-1)[0].squars)
-    const win:boolean = isWin(deepLatestBoard)
-    console.log('isWin',win);
-    if(win){
-      return change.after.ref.set(
-        { winner: true},
-        { merge: true }
-        )
-      }else{
-        return undefined
-      }
+    const deepLatestBoard = deepen(3, 3, data.history.slice(-1)[0].squars)
+    if (deepLatestBoard === undefined) return
+    const win: boolean = isWin(deepLatestBoard)
+    console.log('isWin', win)
+    if (win) {
+      return change.after.ref.set({ winner: true }, { merge: true })
+    }
   })
 
-function isWin(deepArray:any):boolean {
+function isWin(deepArray: matrix[][]): boolean {
   if (!deepArray) return false
   return winCombinations.some((winCombination) => {
-    const fulfill:boolean = winCombination.every((rowCol) => {
+    const fulfill: boolean = winCombination.every((rowCol) => {
       return deepArray[rowCol[0]][rowCol[1]].value.length
     })
-    if (!fulfill) return
+    if (!fulfill) return false
 
     const [row, col] = winCombination[0]
     const [row1, col1] = winCombination[1]
     const [row2, col2] = winCombination[2]
     const matrix0 = deepArray[row][col].value.slice(-1)[0].player
-    const matrix1 =deepArray[row1][col1].value.slice(-1)[0]
-      .player
-    const matrix2 =deepArray[row2][col2].value.slice(-1)[0]
-      .player
+    const matrix1 = deepArray[row1][col1].value.slice(-1)[0].player
+    const matrix2 = deepArray[row2][col2].value.slice(-1)[0].player
     return matrix0 && matrix0 === matrix1 && matrix0 === matrix2
   })
 }
 
-type matrix ={
-  row:number,
-  col:number,
-  value:object[]
+type piece = {
+  id: number
+  number: number
+  player: string
 }
 
-function deepen(row:number,col:number,shallowArray:matrix[]):object[][]| undefined{
-    /**
-     * latestBoardをDeep Arrayに変換する。
-     * latestBoardが[]の場合はundefinedを返す
-     * @type {Array | undefined}
-     */
-    const latestBoard = shallowArray
-    if (!latestBoard.length) return undefined
+interface matrix {
+  row: number
+  col: number
+  value: piece[]
+}
 
-    const arr = new Array(row).fill(undefined).map(() => new Array(0))
-    latestBoard.forEach((obj) => {
-      arr[obj.row].push(obj)
-    })
+type shallow = matrix[]
 
-    return arr
+function deepen(
+  row: number,
+  col: number,
+  shallowArray: shallow
+): matrix[][] | undefined {
+  /**
+   * latestBoardをDeep Arrayに変換する。
+   * latestBoardが[]の場合はundefinedを返す
+   * @type {Array | undefined}
+   */
+  const latestBoard = shallowArray
+  if (!latestBoard.length) return undefined
+
+  const arr = new Array(row).fill(undefined).map(() => new Array(0))
+  latestBoard.forEach((obj) => {
+    arr[obj.row].push(obj)
+  })
+
+  return arr
 }
