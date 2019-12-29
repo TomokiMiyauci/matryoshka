be150e44c7a97a4eef9d75affb9cb455337fd407
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-12-29 21:16:40 +0900

    :sparkles: (pages/playrooms/_uid) add watcher for players are ready or not

--- src/pages/playrooms/_uid.vue
+++ src/pages/playrooms/_uid.vue
@@ -1,51 +1,46 @@
 <template>
-  <v-container class="fill-height">
-    <!-- <div class="text-center">Your Turn</div> -->
-    <!-- <v-wait-sign @up="TIME_UP()" :isYourTurn="isYourTurn"></v-wait-sign> -->
-
-    <the-playground></the-playground>
-
-    <v-container style="padding:0;margin:0;">
-      <v-row justify="center" align="center" align-content="center">
-        <v-col v-for="piece in holdingPieces" :key="piece.id" cols="auto">
-          <v-btn @click="onCli(piece)">{{ piece.value }}</v-btn>
-        </v-col>
-      </v-row>
-    </v-container>
-
-    <p>{{ deepLatestBoard }}</p>
-
-    <v-dialog :value="victoryOrDefeat" persistent max-width="290">
-      <v-card>
-        <v-card-title class="headline">{{ title }}</v-card-title>
-        <v-card-text>{{ text }}</v-card-text>
+  <div>
+    <!-- <v-card width="600px" class="card">
+      <v-card-title primary-title>
+        Debug info
+        <v-card-text> </v-card-text>
         <v-card-actions>
-          <v-btn @click="onExit">
-            <v-icon left>mdi-exit-to-app</v-icon>exit</v-btn
-          >
-          <v-spacer></v-spacer>
-          <v-btn color="green darken-1" @click="onClick"
-            ><v-icon left>mdi-sword-cross</v-icon>try again</v-btn
-          >
+          <v-btn @click="reset" color="warning">
+            reset
+          </v-btn>
+        </v-card-actions>
+      </v-card-title>
+    </v-card> -->
+    <the-playground
+      :deep-array="todos"
+      :doc-ref="docRef"
+      ref="playground"
+    ></the-playground>
+    <v-dialog
+      v-model="dialog"
+      scrollable
+      persistent
+      :overlay="false"
+      max-width="500px"
+      transition="dialog-transition"
+    >
+      <v-card
+        ><v-card-title>win</v-card-title>
+        <v-card-actions>
+          <v-btn>exit</v-btn>
+          <v-divider></v-divider>
+          <v-btn @click="ready" color="success">ready</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
-    <!--
-    <v-overlay :value="!isYourTurn">
-      <v-turn></v-turn>
-
-      <v-btn @click="onClickLose" fixed top right color="indigo"
-        >Surrender
-        <v-icon rigth>mdi-handshake</v-icon>
-      </v-btn>
-    </v-overlay>  -->
-  </v-container>
+  </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex'
+import { mapGetters } from 'vuex'
+import firebase, { firestore } from '~/plugins/firebase.js'
 
-import ThePlayground from '~/components/organisms/ThePlayground'
+import ThePlayground from '~/components/organisms/ThePlayground.vue'
 export default {
   layout: 'playroom',
 
@@ -53,91 +48,150 @@ export default {
     ThePlayground
   },
 
-  computed: {
-    ...mapGetters('game', [
-      'turnPlayer',
-      'holdingPieces',
-      'round',
-      'deepLatestBoard'
-    ]),
-    ...mapGetters('modal', ['isOpened']),
-    ...mapGetters('player', ['isYourTurn', 'victoryOrDefeat', 'name']),
+  async fetch({ store }) {
+    await store.dispatch('game/bindGameRef')
+  },
 
-    title() {
-      switch (this.victoryOrDefeat) {
-        case 'WIN':
-          return `Win!`
-        case 'DRAW':
-          return `Draw`
-      }
-      return `Lose...`
+  data() {
+    return {
+      dialog: false
+    }
+  },
+
+  computed: {
+    ...mapGetters('playroom', ['id']),
+    ...mapGetters('game', ['game', 'generateInitValue']),
+    ...mapGetters('player', ['name']),
+
+    lastGame() {
+      return this.game.slice(-1)[0]
     },
 
-    text() {
-      switch (this.victoryOrDefeat) {
-        case 'WIN':
-          return `You are the winner.`
-        case 'DRAW':
-          return `You played a fun game`
+    lastGameId() {
+      return this.lastGame.id
+    },
+
+    latestGame() {
+      const lastGame = this.lastGame
+
+      if ('readyPlayers' in lastGame && lastGame.readyPlayers.length === 2) {
+        return lastGame
+      } else if (this.game.length) {
+        return this.game[this.game.length - 2]
       }
-      return `You are still a loser.`
+      return undefined
+    },
+
+    latestGameId() {
+      const latestGame = this.latestGame
+      if (!latestGame) return undefined
+      return latestGame.id
+    },
+
+    readyPlayers() {
+      return this.lastGame ? this.lastGame.readyPlayers : undefined
+    },
+
+    todos() {
+      return this.latestGame ? this.latestGame : {}
+    },
+
+    round() {
+      return this.game.length ? this.game.length : undefined
     }
   },
 
   watch: {
-    round() {
-      this.INIT_HOLDING_PIECE(this.name)
+    todos(val) {
+      switch (val.winner) {
+        case 'DRAW': {
+          this.dialog = true
+          break
+        }
+
+        case this.name: {
+          this.dialog = true
+          break
+        }
+
+        case this.name === 'PLAYER_1' ? 'PLAYER_2' : 'PLAYER_1': {
+          this.dialog = true
+          break
+        }
+      }
+    },
+
+    dialog(val) {
+      if (!val) {
+        this.init()
+      }
+    },
+
+    readyPlayers(val) {
+      if (
+        val &&
+        this.dialog &&
+        this.lastGame.history.length === 1 &&
+        val.length === 2
+      ) {
+        this.dialog = false
+        this.init()
+      }
     }
   },
 
   created() {
-    // await this.bindPlayroomRef()
-    // await this.bindGameRef()
-  },
-
-  mounted() {
-    this.INIT_HOLDING_PIECE(this.name)
+    this.ready()
   },
 
   methods: {
-    ...mapActions('modal', ['HIDE']),
-    ...mapActions('game', [
-      'RESTART',
-      'NEXT_GAME',
-      'bindGameRef',
-      'TIME_UP',
-      'ADD_SELECTING_PIECE',
-      'INIT_HOLDING_PIECE'
-    ]),
-    ...mapActions('playroom', ['bindPlayroomRef']),
-    ...mapActions('player', ['SURRENDER', 'LEAVE_ROOM']),
+    // async nextGame() {
+    //   await firestore
+    //     .collection('playrooms')
+    //     .doc(this.id)
+    //     .collection('games')
+    //     .doc((this.round + 1).toString())
+    //     .set(this.generateInitValue)
+    //     .catch((error) => {
+    //       console.error('Error adding document: ', error)
+    //     })
 
-    onClick() {
-      this.NEXT_GAME()
-      this.HIDE(1)
+    //   // this.dialog = false
+    //   // this.init()
+    // },
+
+    async ready() {
+      await firestore
+        .collection('playrooms')
+        .doc(this.id)
+        .collection('games')
+        .doc(this.lastGameId)
+        .update({
+          readyPlayers: firebase.firestore.FieldValue.arrayUnion(this.name)
+        })
+        .catch((error) => {
+          console.error('Error adding document: ', error)
+        })
     },
 
-    onExit() {
-      this.HIDE(1)
-      this.LEAVE_ROOM()
-      this.$router.push('/playrooms')
+    docRef(collectionName = 'playrooms') {
+      return firestore
+        .collection(collectionName)
+        .doc(this.id)
+        .collection('games')
+        .doc(this.latestGameId)
     },
 
-    onClickLose() {
-      this.SURRENDER()
-    },
-
-    onCli(payload) {
-      console.log(payload)
-      this.ADD_SELECTING_PIECE({ ...payload, type: 'PLACE' })
+    init() {
+      this.$refs.playground.initHoldingPieces(this.name)
     }
   }
 }
 </script>
 
 <style scoped>
-.cccc {
-  bottom: 0px;
+.card {
   position: absolute;
+  right: 0;
 }
 </style>
