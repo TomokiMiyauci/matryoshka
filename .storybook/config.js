83689a549e46049a5675faf39bd517b5e23ce8da
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-12-29 23:48:59 +0900

    :wrench: (.storybook) apply vue components in typescript

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from '@storybook/vue';
 import Vue from 'vue';
 import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css'; 
+import 'vuetify/dist/vuetify.min.css';
 
 Vue.use(Vuetify);
 
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,6 +1,25 @@
 const path = require('path');
- 
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+
 module.exports = ({ config }) => {
   config.resolve.alias['~'] = path.resolve(__dirname, '../src')
+  config.resolve.extensions.push('.ts')
+
+  config.module.rules.push({
+    test: /\.ts$/,
+    exclude: /node_modules/,
+    use: [
+      {
+        loader: 'ts-loader',
+        options: {
+          appendTsSuffixTo: [/\.vue$/],
+          transpileOnly: true
+        }
+      }
+    ]
+  })
+
+  config.plugins.push(new ForkTsCheckerWebpackPlugin())
+
   return config
 }
