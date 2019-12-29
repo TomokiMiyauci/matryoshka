f6ca37a2cc05f178340218dc3b0fb9e19e34a823
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2019-12-29 23:51:08 +0900

    :wrench: apply to test with vuetify components

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,16 @@
 module.exports = {
+  setupFilesAfterEnv: ['./test/setup.js'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '^~/(.*)$': '<rootDir>/src/$1',
     '^vue$': 'vue/dist/vue.common.js'
   },
-  moduleFileExtensions: ['js', 'vue', 'json'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ['js', 'vue', 'ts', 'json'],
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
+    '.*\\.(vue)$': 'vue-jest',
+    '^.+\\.tsx?$': 'ts-jest'
   },
   collectCoverage: true,
   collectCoverageFrom: [
