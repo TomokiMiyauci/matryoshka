2fc5a50b92c0c611e89855cad3ab5d26d1bdb8a9
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-01-11 01:36:34 +0900

    :recycle: (modelules/VBoard) change element table to div, compositionable, remove unused feature

--- src/components/molecules/VBoard.vue
+++ src/components/molecules/VBoard.vue
@@ -1,210 +1,126 @@
 <template>
-  <table style="margin:0 auto;">
-    <tbody>
-      <tr v-for="(rows, index) in board" :key="index">
-        <td
-          v-for="cols in rows"
-          :key="cols.row + cols.col"
-          :class="classStyle(cols)"
-          @click="$emit('click', cols)"
-          class="td"
+  <div class="grid">
+    <div v-for="(rows, index) in matrix" :key="index" class="rows">
+      <div
+        v-for="element in rows"
+        :key="element.row + element.col"
+        class="cols"
+      >
+        <v-square
+          :placeable="isPlaceable(element)"
+          :is-selecting="isSelecting(element)"
+          :element="element"
+          @click="$emit('click', element)"
         >
-          <v-doll :color="color(cols)" :width="calcWidth(cols)"></v-doll>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+          <v-doll v-bind="dollProps(element)"></v-doll>
+        </v-square>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { createComponent } from '@vue/composition-api'
+import { createComponent, computed, reactive, ref } from '@vue/composition-api'
 import VDoll from '~/components/atoms/VDoll.vue'
-// eslint-disable-next-line no-unused-vars
-import { Piece } from '~/types/piece'
-
+import VSquare from '~/components/molecules/VSquare.vue'
+import { Matrix, Element } from '~/types/piece'
 type Props = {
-  holding: Holding
-  nextNumber: number
-  shallowBoard: []
+  matrix: Matrix
+  nextValue: number
+  holding: Element
 }
 
-type Holding = {
-  row: number
-  col: number
-}
-
-type Board = Entry[]
-
-interface Entry {
-  row: number
-  col: number
-  value: Piece[]
-}
-
-function placeableBoard(value: number, board: Board): Board | undefined {
-  /**
-   * プレイス可能な現在のボードを返す
-   */
-  if (!board) {
-    return undefined
+const useHolding = (props: Props) => {
+  const isPlaceable = (element: Element): boolean => {
+    const nextValue = props.nextValue
+    if (nextValue === undefined) {
+      return false
+    }
+    return !element.value.length || element.value.slice(-1)[0].value < nextValue
   }
 
-  const a = board.filter((matrix: Entry) => {
-    const latestValue = matrix.value.length
-      ? matrix.value.slice(-1)[0]
-      : undefined
-
-    if (!latestValue || latestValue.value < value) {
-      return true
+  const isSelecting = (element: Element): boolean => {
+    if (!props.holding) {
+      return false
     }
-  })
-  return a
+    return (
+      props.holding.row === element.row && props.holding.col === element.col
+    )
+  }
+
+  return {
+    isPlaceable,
+    isSelecting
+  }
+}
+
+const useDollProps = () => {
+  const dollProps = (element: Element) => {
+    const latestValue = element.value.length
+      ? element.value.slice(-1)[0]
+      : undefined
+    if (latestValue === undefined) return {}
+
+    const color = latestValue.player === 'PLAYER_1' ? 'red' : 'blue'
+    const BASE_SIZE = 33
+    const size = `${BASE_SIZE + latestValue.value * BASE_SIZE}px`
+    return { color, width: size }
+  }
+
+  return {
+    dollProps
+  }
 }
 
 export default createComponent({
   props: {
-    board: {
+    matrix: {
       type: Array,
-      require: true
+      required: true
     },
-
-    holding: {
-      type: Object,
-      require: true
-    },
-
-    nextNumber: {
+    nextValue: {
       type: Number,
       require: true
     },
-
-    shallowBoard: {
-      type: Array,
+    holding: {
+      type: Object,
       require: true
     }
   },
 
   components: {
+    VSquare,
     VDoll
   },
 
   setup(props: Props) {
-    const color = (element: Entry): string | undefined => {
-      if (ownPlayer1(element)) {
-        return 'red'
-      } else if (ownPlayer2(element)) {
-        return 'blue'
-      }
-      return undefined
-    }
+    const { isPlaceable, isSelecting } = useHolding(props)
 
-    const calcWidth = (element: Entry): string => {
-      const value = element.value.length
-        ? element.value.slice(-1)[0]
-        : undefined
-      if (value === undefined) return ''
-      const BASE_SIZE = 33
-      return `${BASE_SIZE + value.value * BASE_SIZE}px`
-    }
+    const { dollProps } = useDollProps()
 
-    const isPlaceable = (matrix: Entry): boolean => {
-      /**
-       * プレイス可能かどうかを判定する
-       */
-      const nextNumber = props.nextNumber
-
-      if (nextNumber === undefined) {
-        return false
-      }
-
-      const placeable = placeableBoard(nextNumber, props.shallowBoard)
-      if (!placeable) return false
-
-      return placeable.some((a) => a.row === matrix.row && a.col === matrix.col)
-    }
-
-    const isSelecting = (matrix: Entry): boolean => {
-      /**
-       * 選択中かどうかを判定する
-       */
-      if (!props.holding) {
-        return false
-      }
-      if (
-        props.holding.row === matrix.row &&
-        props.holding.col === matrix.col
-      ) {
-        return true
-      }
-      return false
-    }
-
-    const ownPlayer1 = (matrix: Entry): boolean => {
-      const { value } = matrix
-      if (!value.length || value.slice(-1)[0].player !== 'PLAYER_1')
-        return false
-      return true
-    }
-
-    const ownPlayer2 = (matrix: Entry): boolean => {
-      const { value } = matrix
-      if (!value.length || value.slice(-1)[0].player !== 'PLAYER_2')
-        return false
-      return true
-    }
-
-    const classStyle = (entry: Entry) => {
-      return {
-        'td-placeable': isPlaceable(entry),
-        'td-selecting': isSelecting(entry),
-        'own-player1': ownPlayer1(entry),
-        'own-player2': ownPlayer2(entry)
-      }
-    }
-
-    return {
-      classStyle,
-      color,
-      calcWidth
-    }
+    return { dollProps, isPlaceable, isSelecting }
   }
 })
 </script>
 
 <style scoped>
-.td {
-  width: 100px;
-  height: 100px;
-  background: rgba(88, 38, 167, 0.356);
-  border-radius: 10px;
-  border: 1px solid #fff;
-  text-align: center;
-  vertical-align: middle;
+.grid {
+  margin: 0 auto;
+  width: 70vw;
+  max-width: 50vh;
+  height: 70vw;
+  max-height: 50vh;
 }
-.td-placeable {
-  animation: placeable 0.8s infinite alternate;
-  border: 2px double #fff;
+.rows {
+  display: flex;
 }
-.td-placeable:hover {
-  cursor: pointer;
-  background: rgba(164, 5, 238, 0.411);
+.cols {
+  flex: 1 0 auto;
+  position: relative;
 }
-.td-selecting {
-  border-color: yellow;
-  background-color: rgba(255, 255, 255, 0.8);
-}
-@keyframes placeable {
-  0% {
-  }
-  100% {
-    box-shadow: 1px 1px 10px 5px rgba(238, 192, 67, 0.692);
-    background-color: rgba(151, 150, 49, 0.3);
-  }
-}
-.own-player1 {
-  color: rgba(223, 37, 37, 0.8);
-}
-.own-player2 {
-  color: rgba(37, 71, 223, 0.8);
+.cols::before {
+  content: '';
+  float: left;
+  padding-top: 100%;
 }
 </style>
