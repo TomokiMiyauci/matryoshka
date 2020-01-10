453eaf8e7d3963228e57f612b1ea80f3746ccc3f
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-01-10 23:29:51 +0900

    :recycle: (molecules/VSquare) add composition feature

--- src/components/molecules/VSquare.vue
+++ src/components/molecules/VSquare.vue
@@ -1,17 +1,62 @@
 <template>
-  <div class="square" @click="$emit('click', piece)">
+  <div class="square" :class="classStyle" @click="$emit('click', element)">
     <slot></slot>
   </div>
 </template>
 
-<script>
-import { createComponent } from '@vue/composition-api'
+<script lang="ts">
+import { createComponent, computed } from '@vue/composition-api'
+import { Element, Player } from '~/types/piece'
+type Props = {
+  element: Element
+  placeable: boolean
+  isSelecting: boolean
+}
+
+const useStyle = (props: Props) => {
+  const isOwn = (element: Element, player: Player): boolean => {
+    const { value } = element
+    if (!value.length) return false
+    return value.slice(-1)[0].player === player
+  }
+
+  const classStyle = computed(() => {
+    return {
+      'own-player1': isOwn(props.element, 'PLAYER_1'),
+      'own-player2': isOwn(props.element, 'PLAYER_2'),
+      glow: props.placeable,
+      'is-selecting': props.isSelecting
+    }
+  })
+
+  return {
+    classStyle
+  }
+}
 
 export default createComponent({
   props: {
-    piece: {
+    element: {
       type: Object,
       require: true
+    },
+
+    placeable: {
+      type: Boolean,
+      require: true
+    },
+
+    isSelecting: {
+      type: Boolean,
+      require: true
+    }
+  },
+
+  setup(props: Props) {
+    const { classStyle } = useStyle(props)
+
+    return {
+      classStyle
     }
   }
 })
@@ -25,10 +70,63 @@ export default createComponent({
   bottom: 0;
   top: 0;
   margin: 1px;
-  background-color: rgba(47, 150, 69, 0.795);
-  border-radius: 5%;
+  background-color: rgba(85, 69, 173, 0.8);
+  border-radius: 10%;
+  border: 1px solid rgba(128, 128, 128, 0.5);
   display: flex;
   align-items: center;
   justify-content: center;
+}
+
+.glow {
+  animation: glow 0.8s infinite alternate;
+}
+
+@keyframes glow {
+  0% {
+  }
+  100% {
+    box-shadow: 1px 1px 10px 5px rgba(238, 192, 67, 0.692);
+    background-color: rgba(151, 150, 49, 0.3);
+  }
+}
+.glow:hover {
+  cursor: pointer;
+  background-color: rgba(164, 5, 238, 0.411) !important;
+}
+
+.is-selecting {
+  cursor: pointer;
+  background-color: rgba(94, 5, 238, 0.3);
+  animation: ShineAnimation 3s infinite cubic-bezier(0.12, 0.89, 0.98, 0.47);
+}
+
+@keyframes ShineAnimation {
+  0% {
+    background-repeat: no-repeat;
+    background-image: linear-gradient(
+      top left,
+      rgba(255, 255, 255, 0) 0%,
+      rgba(255, 255, 255, 0) 20%,
+      rgba(255, 255, 255, 0.5) 48%,
+      rgba(255, 255, 255, 0.8) 50%,
+      rgba(255, 255, 255, 0.5) 52%,
+      rgba(255, 255, 255, 0) 57%,
+      rgba(255, 255, 255, 0) 100%
+    );
+    background-position: -250px -250px;
+    background-size: 500px 480px;
+  }
+  100% {
+    background-repeat: no-repeat;
+    background-position: 250px 250px;
+  }
+}
+
+.own-player1 {
+  color: rgba(223, 37, 37, 0.8);
+}
+.own-player2 {
+  color: rgba(37, 71, 223, 0.8);
 }
 </style>
