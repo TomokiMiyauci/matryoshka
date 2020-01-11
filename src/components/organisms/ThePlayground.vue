202fc2f6b1ee144f2259c1e5534121fb09997484
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-01-12 03:31:52 +0900

    :recycle: (organisms/ThePlayground) separate functions, externalize as typescript

--- src/components/organisms/ThePlayground.vue
+++ src/components/organisms/ThePlayground.vue
@@ -13,102 +13,20 @@ import firebase, { firestore } from '~/plugins/firebase.js'
 import VBoard from '~/components/molecules/VBoard.vue'
 import VPossession from '~/components/molecules/VPossession.vue'
 import VGameManager from '~/components/molecules/VGameManager.vue'
-import { generatePieces } from '~/compositions/piece'
+import {
+  generatePieces,
+  useHoldingPieces,
+  usePlacePiece,
+  useMovePiece
+} from '~/compositions/piece'
 import { reshape } from '~/compositions/matrix'
-import { Piece, Element, PieceMotion, Player, Game } from '~/types/piece'
+import { Piece, Element, Player, Game } from '~/types/piece'
 
 type Props = {
   game: Game
   docRef: any
   isYourTurn: boolean
 }
-
-type From = {
-  row: number
-  col: number
-}
-
-type SelectingPiece = {
-  pieceMotion: PieceMotion
-  from?: From
-  piece: Piece
-}
-
-const usePlacePiece = () => {
-  const placePieceRef = ref<SelectingPiece>()
-
-  const mutate = (piece: Piece) => {
-    if (
-      placePieceRef.value === undefined ||
-      placePieceRef.value.piece !== piece
-    ) {
-      placePieceRef.value = { pieceMotion: 'PLACE', piece }
-    } else {
-      clear()
-    }
-  }
-
-  const clear = () => {
-    placePieceRef.value = undefined
-  }
-
-  return { placePieceRef, mutate, clear }
-}
-
-const useMovePiece = () => {
-  const movePieceRef = ref<SelectingPiece>()
-
-  const mutate = (element: Element) => {
-    const { row, col, value } = element
-    const piece = value.slice(-1)[0]
-
-    if (
-      movePieceRef.value === undefined ||
-      (movePieceRef.value.from &&
-        movePieceRef.value.from.row === element.row &&
-        movePieceRef.value.from.col === element.col)
-    ) {
-      movePieceRef.value = {
-        pieceMotion: 'MOVE',
-        from: { row, col },
-        piece
-      }
-    } else {
-      clear()
-    }
-  }
-
-  const clear = () => {
-    movePieceRef.value = undefined
-  }
-
-  return {
-    movePieceRef,
-    mutate,
-    clear
-  }
-}
-
-export const useHoldingPieces = () => {
-  const holdingPiecesRef = ref<Piece[]>()
-
-  const init = (row: number, col: number, inRow: number, player: Player) => {
-    holdingPiecesRef.value = generatePieces(row, col, inRow, player)
-  }
-
-  const pop = (piece: Piece) => {
-    if (holdingPiecesRef.value === undefined) return
-    const index = holdingPiecesRef.value.findIndex((holdingPiece) => {
-      return holdingPiece.id === piece.id && holdingPiece.value === piece.value
-    })
-
-    holdingPiecesRef.value.splice(index, 1)
-  }
-
-  return { holdingPiecesRef, init, pop }
-}
-
-const setup = () => {}
 
 export default createComponent({
   components: {
@@ -151,15 +69,18 @@ export default createComponent({
     const player: Player = root.$store.getters['player/name']
 
     init(props.game.rows, props.game.cols, 3, player)
+
     const matrix = computed(() => {
       return reshape(props.game.history.slice(-1)[0].squares, props.game.rows)
     })
 
-    const nextValue = computed(() => {
-      const a = placePieceRef.value || movePieceRef.value
+    const selectingPiece = computed(() => {
+      return placePieceRef.value || movePieceRef.value
+    })
 
-      if (a === undefined) return
-      return a.piece.value
+    const nextValue = computed(() => {
+      if (selectingPiece.value === undefined) return
+      return selectingPiece.value.piece.value
     })
 
     const boardProps = computed(() => {
@@ -182,7 +103,7 @@ export default createComponent({
       return {
         isYourTurn: props.isYourTurn,
         player,
-        nextPlayer: 'PLAYER_1'
+        nextPlayer: props.game.nextPlayer
       }
     })
 
@@ -198,13 +119,7 @@ export default createComponent({
       if (!props.isYourTurn) return
       const { row, col, value } = element
 
-      if (
-        placePieceRef.value === undefined &&
-        movePieceRef.value === undefined &&
-        !value.length
-      ) {
-        return
-      }
+      if (selectingPiece.value === undefined) return
 
       if (
         placePieceRef.value !== undefined &&
@@ -216,12 +131,7 @@ export default createComponent({
         )
           return
 
-        const matrix = reshape(
-          props.game.history.slice(-1)[0].squares,
-          props.game.rows
-        )
-
-        const cloneMatrix = cloneDeep(matrix)
+        const cloneMatrix = cloneDeep(matrix.value)
         cloneMatrix[row][col].value.push(placePieceRef.value.piece)
 
         props.docRef().update({
@@ -242,12 +152,7 @@ export default createComponent({
       ) {
         const { piece, from } = movePieceRef.value
         if (!value.length || value.slice(-1)[0].value < piece.value) {
-          const matrix = reshape(
-            props.game.history.slice(-1)[0].squares,
-            props.game.rows
-          )
-
-          const cloneMatrix = cloneDeep(matrix)
+          const cloneMatrix = cloneDeep(matrix.value)
           cloneMatrix[row][col].value.push(movePieceRef.value.piece)
           const { row: frow, col: fcol } = from
           cloneMatrix[frow][fcol].value.pop()
