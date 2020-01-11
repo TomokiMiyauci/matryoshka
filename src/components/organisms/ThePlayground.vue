fc3651d74b9b76b92562adf7129de76fc6b0cd87
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-01-12 02:44:11 +0900

    :recycle: (organisms/ThePlayground) apply composition api, typescript

--- src/components/organisms/ThePlayground.vue
+++ src/components/organisms/ThePlayground.vue
@@ -1,30 +1,124 @@
 <template>
   <div>
-    <v-game-manager
-      :is-your-turn="isYourTurn"
-      :player="name"
-      :next-player="nextPlayer"
-    ></v-game-manager>
+    <v-game-manager v-bind="gameManagerProps"></v-game-manager>
     <v-board @click="onClick" v-bind="boardProps"></v-board>
     <v-possession v-bind="possessionProps" @click="click"></v-possession>
   </div>
 </template>
 
-<script>
-import { mapGetters } from 'vuex'
-import firebase from '~/plugins/firebase.js'
+<script lang="ts">
+import { createComponent, ref, computed } from '@vue/composition-api'
+import cloneDeep from 'lodash/cloneDeep'
+import firebase, { firestore } from '~/plugins/firebase.js'
 import VBoard from '~/components/molecules/VBoard.vue'
 import VPossession from '~/components/molecules/VPossession.vue'
 import VGameManager from '~/components/molecules/VGameManager.vue'
+import { generatePieces } from '~/compositions/piece'
+import { reshape } from '~/compositions/matrix'
+import { Piece, Element, PieceMotion, Player, Game } from '~/types/piece'
 
-export default {
+type Props = {
+  game: Game
+  docRef: any
+  isYourTurn: boolean
+}
+
+type From = {
+  row: number
+  col: number
+}
+
+type SelectingPiece = {
+  pieceMotion: PieceMotion
+  from?: From
+  piece: Piece
+}
+
+const usePlacePiece = () => {
+  const placePieceRef = ref<SelectingPiece>()
+
+  const mutate = (piece: Piece) => {
+    if (
+      placePieceRef.value === undefined ||
+      placePieceRef.value.piece !== piece
+    ) {
+      placePieceRef.value = { pieceMotion: 'PLACE', piece }
+    } else {
+      clear()
+    }
+  }
+
+  const clear = () => {
+    placePieceRef.value = undefined
+  }
+
+  return { placePieceRef, mutate, clear }
+}
+
+const useMovePiece = () => {
+  const movePieceRef = ref<SelectingPiece>()
+
+  const mutate = (element: Element) => {
+    const { row, col, value } = element
+    const piece = value.slice(-1)[0]
+
+    if (
+      movePieceRef.value === undefined ||
+      (movePieceRef.value.from &&
+        movePieceRef.value.from.row === element.row &&
+        movePieceRef.value.from.col === element.col)
+    ) {
+      movePieceRef.value = {
+        pieceMotion: 'MOVE',
+        from: { row, col },
+        piece
+      }
+    } else {
+      clear()
+    }
+  }
+
+  const clear = () => {
+    movePieceRef.value = undefined
+  }
+
+  return {
+    movePieceRef,
+    mutate,
+    clear
+  }
+}
+
+export const useHoldingPieces = () => {
+  const holdingPiecesRef = ref<Piece[]>()
+
+  const init = (row: number, col: number, inRow: number, player: Player) => {
+    holdingPiecesRef.value = generatePieces(row, col, inRow, player)
+  }
+
+  const pop = (piece: Piece) => {
+    if (holdingPiecesRef.value === undefined) return
+    const index = holdingPiecesRef.value.findIndex((holdingPiece) => {
+      return holdingPiece.id === piece.id && holdingPiece.value === piece.value
+    })
+
+    holdingPiecesRef.value.splice(index, 1)
+  }
+
+  return { holdingPiecesRef, init, pop }
+}
+
+const setup = () => {}
+
+export default createComponent({
   components: {
     VGameManager,
     VBoard,
     VPossession
   },
+
   props: {
-    deepArray: {
+    game: {
       type: Object,
       required: true
     },
@@ -40,271 +134,153 @@ export default {
     }
   },
 
-  data: () => ({
-    row: 3,
-    col: 3,
-    selecting: undefined,
-    holding: undefined,
-    holdingPieces: [],
-    turn: 1
-  }),
+  setup(props: Props, { root }) {
+    const {
+      placePieceRef,
+      mutate: mutatePlacePiece,
+      clear: clearPlacePiece
+    } = usePlacePiece()
 
-  computed: {
-    ...mapGetters('playroom', ['id']),
-    ...mapGetters('player', ['name']),
+    const {
+      movePieceRef,
+      mutate: mutateMovePiece,
+      clear: clearMovePiece
+    } = useMovePiece()
 
-    boardProps() {
-      const { deepLatestBoard, nextNumber, holding, latestBoard } = this
+    const { holdingPiecesRef, init, pop } = useHoldingPieces()
+    const player: Player = root.$store.getters['player/name']
+
+    init(props.game.rows, props.game.cols, 3, player)
+    const matrix = computed(() => {
+      return reshape(props.game.history.slice(-1)[0].squares, props.game.rows)
+    })
+
+    const nextValue = computed(() => {
+      const a = placePieceRef.value || movePieceRef.value
+
+      if (a === undefined) return
+      return a.piece.value
+    })
+
+    const boardProps = computed(() => {
       return {
-        board: deepLatestBoard,
-        nextNumber,
-        holding,
-        shallowBoard: latestBoard
+        matrix: matrix.value,
+        nextValue: nextValue.value,
+        holding: movePieceRef.value ? movePieceRef.value.piece : undefined
       }
-    },
+    })
 
-    possessionProps() {
-      const { holdingPieces, selecting, name } = this
+    const possessionProps = computed(() => {
       return {
-        holdingPieces,
-        selecting,
-        player: name
+        holdingPieces: holdingPiecesRef.value,
+        selecting: placePieceRef.value ? placePieceRef.value.piece : undefined,
+        player
       }
-    },
+    })
 
-    nextPlayer() {
-      if (!this.deepArray) return undefined
-      return this.deepArray.nextPlayer
-    },
-
-    latestBoard() {
-      const todos = this.todos
-      return todos && todos.history
-        ? this.cloneDeep(todos.history.slice(-1)[0].squares)
-        : []
-    },
-
-    todos() {
-      return this.deepArray ? this.deepArray : undefined
-    },
-
-    selectingOrHolding() {
-      /**
-       * stateのselectingもしくはholdingのうち、undefinedでないものを返す。
-       * どちらもundefinedの場合はundefinedを返す
-       * @type {Object | undefined}
-       */
-      return this.selecting || this.holding
-    },
-
-    deepLatestBoard() {
-      /**
-       * latestBoardをDeep Arrayに変換する。
-       * latestBoardが[]の場合はundefinedを返す
-       * @type {Array | undefined}
-       */
-      const latestBoard = this.latestBoard
-      if (!latestBoard.length) {
-        return undefined
+    const gameManagerProps = computed(() => {
+      return {
+        isYourTurn: props.isYourTurn,
+        player,
+        nextPlayer: 'PLAYER_1'
       }
+    })
 
-      const arr = new Array(this.row).fill(undefined).map(() => new Array(0))
-      latestBoard.forEach((matrix) => {
-        arr[matrix.row].push(matrix)
-      })
-
-      return arr
-    },
-
-    nextNumber() {
-      const b = this.selectingOrHolding
-
-      if (!b) {
-        return undefined
+    const click = (piece: Piece) => {
+      if (!props.isYourTurn) return
+      if (movePieceRef.value) {
+        clearMovePiece()
       }
-      if ('number' in b) {
-        return b.number
-      } else if ('value' in b) {
-        return b.value.slice(-1)[0].number
-      }
-      return undefined
+      mutatePlacePiece(piece)
     }
-  },
 
-  created() {
-    this.initHoldingPieces(this.name)
-  },
+    const onClick = (element: Element) => {
+      if (!props.isYourTurn) return
+      const { row, col, value } = element
 
-  methods: {
-    initHoldingPieces(playerName) {
-      if (!playerName) return
-      const holdingPieces = [
-        { id: 0, number: 0, player: playerName },
-        { id: 1, number: 0, player: playerName },
-        { id: 2, number: 1, player: playerName },
-        { id: 3, number: 1, player: playerName },
-        { id: 4, number: 2, player: playerName },
-        { id: 5, number: 2, player: playerName }
-      ]
-      this.holdingPieces = holdingPieces
-    },
-    filter(deepArray, player) {
-      if (!deepArray) return []
-      return deepArray.map((matrix) => {
-        return matrix.filter((value) => {
-          if (!('value' in value) | !value.value.length) return
-
-          return value.value.slice(-1)[0].player === player
-        })
-      })
-    },
-
-    click(value) {
-      if (!this.isYourTurn) return
-      if (this.holding) {
-        this.holding = undefined
-      }
-      value === this.selecting
-        ? (this.selecting = undefined)
-        : (this.selecting = value)
-    },
-
-    cloneDeep(value) {
-      return JSON.parse(JSON.stringify(value))
-    },
-
-    getMetrix({ row, col }, board) {
-      const filtered = board.filter((metrix) => {
-        return metrix.row === row && metrix.col === col
-      })
-
-      return filtered[0]
-    },
-
-    getPlacedBoard({ row, col }, update, board) {
-      return board.map((matrix) => {
-        if (matrix.col === col && matrix.row === row) {
-          const { value } = matrix
-          value.push(update)
-
-          return { ...matrix, value }
-        } else {
-          return matrix
-        }
-      })
-    },
-
-    getLatestValue({ row, col }, board) {
-      const matrix = this.getMetrix({ row, col }, board)
-      return matrix.value ? matrix.value.slice(-1)[0] : {}
-    },
-
-    getMovedBoard(from, to, board) {
-      const latestValue = this.getLatestValue(from, board)
-      if (!latestValue) {
-        return []
-      }
-
-      const movedBoard = board.map((matrix) => {
-        if (matrix.col === from.col && matrix.row === from.row) {
-          const { value } = matrix
-          value.pop()
-          return { ...matrix, value }
-        } else if (matrix.col === to.col && matrix.row === to.row) {
-          const { value } = matrix
-          value.push(latestValue)
-          return { ...matrix, value }
-        } else {
-          return matrix
-        }
-      })
-      return movedBoard
-    },
-
-    onClick(payload) {
-      if (!this.isYourTurn) return
-      const { row, col, value } = payload
-
-      if (!this.selecting && !this.holding && !value.length) {
+      if (
+        placePieceRef.value === undefined &&
+        movePieceRef.value === undefined &&
+        !value.length
+      ) {
         return
       }
 
-      if (this.selecting) {
-        // not placeable
-        if (value.length) {
-          if (value.slice(-1)[0].number >= this.selecting.number) {
-            // const selectedValue = this.cloneDeep(this.selecting)
-            // this.selecting = undefined
-            // console.log(1111, selectedValue)
-
-            // this.holding = selectedValue
-
-            return
-          }
-        }
-
-        this.place({ row, col }, this.selecting, this.latestBoard)
-        const a = this.holdingPieces.filter((holdingPiece) => {
-          return this.selecting.id !== holdingPiece.id
-        })
-
-        this.selecting = undefined
-        this.holdingPieces = a
-      } else if (this.holding) {
+      if (
+        placePieceRef.value !== undefined &&
+        placePieceRef.value.pieceMotion === 'PLACE'
+      ) {
         if (
-          this.holding.row !== payload.row ||
-          this.holding.col !== payload.col
-        ) {
-          const { value, ...from } = this.holding
-          if (
-            !payload.value.length ||
-            payload.value.slice(-1)[0].number < value.slice(-1)[0].number
-          ) {
-            this.move(from, { row, col }, this.latestBoard)
-          } else {
-            return
-          }
+          !!value.length &&
+          value.slice(-1)[0].value >= placePieceRef.value.piece.value
+        )
+          return
+
+        const matrix = reshape(
+          props.game.history.slice(-1)[0].squares,
+          props.game.rows
+        )
+
+        const cloneMatrix = cloneDeep(matrix)
+        cloneMatrix[row][col].value.push(placePieceRef.value.piece)
+
+        props.docRef().update({
+          history: firebase.firestore.FieldValue.arrayUnion({
+            squares: cloneMatrix.flat(),
+            turn: 1,
+            player
+          }),
+          nextPlayer: player === 'PLAYER_1' ? 'PLAYER_2' : 'PLAYER_1'
+        })
+
+        pop(placePieceRef.value.piece)
+        clearPlacePiece()
+      } else if (
+        movePieceRef.value !== undefined &&
+        movePieceRef.value.from !== undefined &&
+        movePieceRef.value.pieceMotion === 'MOVE'
+      ) {
+        const { piece, from } = movePieceRef.value
+        if (!value.length || value.slice(-1)[0].value < piece.value) {
+          const matrix = reshape(
+            props.game.history.slice(-1)[0].squares,
+            props.game.rows
+          )
+
+          const cloneMatrix = cloneDeep(matrix)
+          cloneMatrix[row][col].value.push(movePieceRef.value.piece)
+          const { row: frow, col: fcol } = from
+          cloneMatrix[frow][fcol].value.pop()
+
+          props.docRef().update({
+            history: firebase.firestore.FieldValue.arrayUnion({
+              squares: cloneMatrix.flat(),
+              turn: 1,
+              player
+            }),
+            nextPlayer: player === 'PLAYER_1' ? 'PLAYER_2' : 'PLAYER_1'
+          })
         }
-        this.holding = undefined
+        // else {
+        // }
+
+        clearMovePiece()
       } else {
-        if (value.slice(-1)[0].player !== this.name) return
-
-        this.holding = { ...payload }
+        if (value.slice(-1)[0].player !== player) return
+        mutateMovePiece(element)
       }
-    },
+    }
 
-    move(from, to, board) {
-      const b = this.getMovedBoard(from, to, board)
-      if (!b.length) {
-        return
-      }
-
-      this.docRef().update({
-        history: firebase.firestore.FieldValue.arrayUnion({
-          squares: b,
-          turn: this.turn,
-          player: this.name
-        }),
-        nextPlayer: this.name === 'PLAYER_1' ? 'PLAYER_2' : 'PLAYER_1'
-      })
-      this.turn++
-    },
-
-    place({ row, col }, update, board) {
-      const placedBoard = this.getPlacedBoard({ row, col }, update, board)
-
-      this.docRef().update({
-        history: firebase.firestore.FieldValue.arrayUnion({
-          squares: placedBoard,
-          turn: this.turn,
-          player: this.name
-        }),
-        nextPlayer: this.name === 'PLAYER_1' ? 'PLAYER_2' : 'PLAYER_1'
-      })
-      this.turn++
+    return {
+      init,
+      click,
+      onClick,
+      boardProps,
+      possessionProps,
+      gameManagerProps
     }
   }
-}
+})
 </script>
 
 <style></style>
