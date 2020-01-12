34324c66e9e812a7181d466648ba5da44a19a7e2
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-01-12 16:14:35 +0900

    :recycle: (pages/playrooms/_uid) apply composition api and typescript

--- src/pages/playrooms/_uid.vue
+++ src/pages/playrooms/_uid.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
     <the-playground
-      :deep-array="latestGameOrEmpty"
+      :game="latestGame"
       :doc-ref="docRef"
       :is-your-turn="isYourTurn"
       ref="playground"
     ></the-playground>
+
     <v-dialog
-      v-model="dialog"
+      :value="dialogRef"
       scrollable
       persistent
       :overlay="false"
@@ -16,17 +17,42 @@
     >
       <the-card-match-result
         @ready="onReady"
-        v-bind="cardProps"
+        v-bind="message"
       ></the-card-match-result>
     </v-dialog>
   </div>
 </template>
 
-<script>
-import { mapGetters } from 'vuex'
+<script lang="ts">
+import {
+  createComponent,
+  ref,
+  computed,
+  watch,
+  onBeforeMount
+} from '@vue/composition-api'
 import firebase, { firestore } from '~/plugins/firebase.js'
+import { Player, Game } from '~/types/piece'
 
-export default {
+const useDialog = () => {
+  const dialogRef = ref(false)
+
+  const show = () => {
+    dialogRef.value = true
+  }
+
+  const hide = () => {
+    dialogRef.value = false
+  }
+
+  return {
+    dialogRef,
+    show,
+    hide
+  }
+}
+
+export default createComponent({
   layout: 'playroom',
 
   components: {
@@ -35,158 +61,117 @@ export default {
       import('~/components/organisms/TheCardMatchResult.vue')
   },
 
-  async fetch({ store }) {
-    await store.dispatch('game/bindGameRef')
-  },
+  setup(_, { root }) {
+    const { dialogRef, show, hide } = useDialog()
+    // ;(async () => {
+    //   await root.$store.dispatch('game/bindGameRef')
+    //   // dResultを使った処理
+    // })()
 
-  data() {
-    return {
-      dialog: false
-    }
-  },
-
-  computed: {
-    ...mapGetters('playroom', ['id']),
-    ...mapGetters('game', ['game', 'generateInitValue']),
-    ...mapGetters('player', ['name']),
-
-    cardProps() {
-      return this.message
-    },
-
-    lastGame() {
-      return this.game.slice(-1)[0]
-    },
-
-    lastGameId() {
-      return this.lastGame.id
-    },
-
-    latestGame() {
-      const lastGame = this.lastGame
-
-      if ('readyPlayers' in lastGame && lastGame.readyPlayers.length === 2) {
-        return lastGame
-      } else if (this.game.length) {
-        return this.game[this.game.length - 2]
+    const games = computed<Game[]>(() => root.$store.getters['game/game'])
+    const lastGame = computed<Game>(() => games.value.slice(-1)[0])
+    const lastGameId = computed(() => lastGame.value.id)
+    const latestGame = computed<Game>(() => {
+      if (
+        // 'readyPlayers' in lastGame.value &&
+        lastGame.value.readyPlayers.length >= 2
+      ) {
+        return lastGame.value
+      } else if (games.value.length === 1) {
+        return games.value[0]
       }
-      return undefined
-    },
+      return games.value[games.value.length - 2]
+    })
 
-    latestGameId() {
-      const latestGame = this.latestGame
-      if (!latestGame) return undefined
-      return latestGame.id
-    },
+    const latestGameId = computed(() => latestGame.value.id)
+    const player = computed<Player>(() => root.$store.getters['player/name'])
+    const playroomId = computed<string>(
+      () => root.$store.getters['playroom/id']
+    )
 
-    readyPlayers() {
-      return this.lastGame ? this.lastGame.readyPlayers : undefined
-    },
+    const isYourTurn = computed(() => {
+      return latestGame.value.nextPlayer === player.value
+    })
 
-    latestGameOrEmpty() {
-      return this.latestGame ? this.latestGame : {}
-    },
+    const docRef = computed(() => {
+      return firestore
+        .collection('playrooms')
+        .doc(playroomId.value)
+        .collection('games')
+        .doc(lastGameId.value)
+    })
 
-    round() {
-      return this.game.length ? this.game.length : undefined
-    },
-
-    isYourTurn() {
-      if (!this.latestGame || !this.name) return false
-
-      return this.latestGame.nextPlayer === this.name
-    },
-
-    message() {
-      if (!this.latestGame || !this.latestGame.winner) return undefined
-      switch (this.latestGame.winner) {
+    const message = computed(() => {
+      switch (latestGame.value.winner) {
         case 'DRAW': {
           return { title: 'Draw', text: 'It was a good match.' }
         }
-        case this.name: {
+        case player.value: {
           return { title: 'Win!', text: 'Great! You are the winner!' }
         }
-        case this.name === 'PLAYER_1' ? 'PLAYER_2' : 'PLAYER_1': {
+        case player.value === 'PLAYER_1' ? 'PLAYER_2' : 'PLAYER_1': {
           return { title: 'Lose..', text: 'Ah... You are the loser.' }
         }
         default: {
           return undefined
         }
       }
+    })
+
+    const playground = ref<any>()
+    const init = () => {
+      playground.value.init(3, 3, 3, player.value)
     }
-  },
 
-  watch: {
-    latestGame(val) {
-      switch (val.winner) {
-        case 'DRAW': {
-          this.dialog = true
-          break
-        }
-
-        case this.name: {
-          this.dialog = true
-          break
-        }
-
-        case this.name === 'PLAYER_1' ? 'PLAYER_2' : 'PLAYER_1': {
-          this.dialog = true
-          break
-        }
-      }
-    },
-
-    dialog(val) {
-      if (!val) {
-        this.init()
-      }
-    },
-
-    readyPlayers(val) {
-      if (
-        val &&
-        this.dialog &&
-        this.lastGame.history.length === 1 &&
-        val.length === 2
-      ) {
-        this.dialog = false
-        this.init()
-      }
-    }
-  },
-
-  created() {
-    this.onReady()
-  },
-
-  methods: {
-    async onReady() {
-      await firestore
-        .collection('playrooms')
-        .doc(this.id)
-        .collection('games')
-        .doc(this.lastGameId)
+    const onReady = async () => {
+      await docRef.value
         .update({
-          readyPlayers: firebase.firestore.FieldValue.arrayUnion(this.name)
+          readyPlayers: firebase.firestore.FieldValue.arrayUnion(player.value)
         })
         .catch((error) => {
           console.error('Error adding document: ', error)
         })
-    },
+    }
 
-    docRef(collectionName = 'playrooms') {
-      return firestore
-        .collection(collectionName)
-        .doc(this.id)
-        .collection('games')
-        .doc(this.latestGameId)
-    },
+    watch(latestGame, (latestGame) => {
+      switch (latestGame.winner) {
+        case undefined: {
+          break
+        }
 
-    init() {
-      this.$refs.playground.initHoldingPieces(this.name)
+        default: {
+          show()
+        }
+      }
+    })
+
+    watch(dialogRef, (next, prev) => {
+      if (!prev && next) {
+        init()
+      }
+    })
+
+    watch(lastGame, (next, prev) => {
+      if (next.readyPlayers.length === 2) {
+        hide()
+      }
+    })
+
+    onReady()
+
+    return {
+      dialogRef,
+      show,
+      hide,
+      latestGame,
+      docRef,
+      isYourTurn,
+      message,
+      onReady,
+      playground
     }
   }
-}
+})
 </script>
 
 <style scoped>
