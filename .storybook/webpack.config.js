3e99090bf5d70addbc3149aa1611931e9d762c99
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-01-13 13:30:55 +0900

    :wrench: (.storybook/webpack.config.js) add config rules of sass loader

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -26,6 +26,11 @@ module.exports = ({ config }) => {
       }
     ]
   })
+  config.module.rules.push({
+    test: /\.scss$/,
+    use: ['style-loader', 'css-loader', 'sass-loader'],
+    include: path.resolve(__dirname, '../'),
+  });
 
   config.plugins.push(new ForkTsCheckerWebpackPlugin())
 
