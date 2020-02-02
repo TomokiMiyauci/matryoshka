2c31153b79e1d810592bd2fc7459f8b1fa66adc5
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-02-02 18:25:44 +0900

    :recycle: (functions/game.ts) separate functions to matrix

--- src/functions/game.ts
+++ src/functions/game.ts
@@ -1,6 +1,4 @@
-import { Matrix, Element, Player, Piece } from '~/types/piece'
-
-const winCombinations = [
+const winCombinations: [number, number][][] = [
   [
     [0, 0],
     [0, 1],
@@ -43,28 +41,11 @@ const winCombinations = [
   ]
 ]
 
-export function isTopPiece(piece: Piece, player: Player): boolean {
-  return piece.player === player
-}
-
-export function isExistPiece(element: Element): boolean {
-  return !!element.value.length
-}
-
-export function isWin(deepArray: object[][]): boolean {
+export const isWin = (deepArray: (Element | {})[][]): boolean => {
   return winCombinations.some((winCombination) => {
     return winCombination.every((rowCol) => {
       const [row, col] = rowCol
       return Boolean(Object.keys(deepArray[row][col]).length)
-    })
-  })
-}
-
-export function getTerritory(matrix: Matrix, player: Player): object[][] {
-  return matrix.map((rows) => {
-    return rows.map((element) => {
-      if (!isExistPiece(element)) return {}
-      return isTopPiece(element.value.slice(-1)[0], player) ? element : {}
     })
   })
 }
