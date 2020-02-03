bf29cdbc75d287d0ba8145cb5cb55f64b14de379
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-02-03 20:30:03 +0900

    :sparkles: (store/firestore.ts) add async action of create playroom, game and game record

--- src/store/firestore.ts
+++ src/store/firestore.ts
@@ -1,4 +1,9 @@
-import { Module, VuexModule, Mutation } from 'vuex-module-decorators'
+import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators'
+import { useFirestorePlayroom } from '~/repositories/playroom'
+import { useFirestoreGame } from '~/repositories/game'
+import { useFirestoreGameRecord } from '~/repositories/game-record'
+import { generateShallow } from '~/functions/matrix'
+import { generatePieces } from '~/functions/piece'
 
 @Module({
   name: 'firestore',
@@ -23,6 +28,36 @@ export default class Firestore extends VuexModule {
   @Mutation
   setPlayroomId(playroomId: string) {
     this.playroomId = playroomId
+  }
+
+  @Action
+  async initialize() {
+    const { createPlayroom } = useFirestorePlayroom()
+    const playroomDocumentData = await createPlayroom({})
+    this.context.commit('setPlayroomId', playroomDocumentData.id)
+
+    const { createGame } = useFirestoreGame(this.context.getters.getPlayroomId)
+    const gameDocumentData = await createGame({
+      rows: 3,
+      cols: 3,
+      nextPlayer: 'PLAYER1',
+      readyPlayers: ['PLAYER1']
+    })
+    this.context.commit('setGameId', gameDocumentData.id)
+
+    const { createGameRecord } = useFirestoreGameRecord(
+      this.context.getters.getPlayroomId,
+      this.context.getters.getGameId
+    )
+    const shallowArray = generateShallow(3, 3)
+    const player1Hands = generatePieces(3, 3, 3, 'PLAYER1')
+    const player2Hands = generatePieces(3, 3, 3, 'PLAYER2')
+    const gameRecordDocumentData = await createGameRecord({
+      board: shallowArray,
+      player1Hands,
+      player2Hands
+    })
+    this.context.commit('setGameRecordId', gameRecordDocumentData.id)
   }
 
   get getPlayroomId() {
