3cdf68bbb35327223bb5df5156a21b370e14c33d
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-02-08 17:14:30 +0900

    :recycle: (organisms/ThePlayground.vue) separate business login, import composition functions

--- src/components/organisms/ThePlayground.vue
+++ src/components/organisms/ThePlayground.vue
@@ -1,101 +1,198 @@
 <template>
   <div>
-    <v-game-manager v-bind="gameManagerProps"></v-game-manager>
-    <v-board @click="onClick" v-bind="boardProps"></v-board>
-    <v-possession v-bind="possessionProps" @click="click"></v-possession>
+    <v-game-manager
+      :enable-timer="enableTimer"
+      v-bind="gameManagerProps"
+      @timeup="defeat"
+    ></v-game-manager>
+    <v-hands
+      v-if="enableEnemyHands"
+      :is-your-hands="false"
+      v-bind="enemyHandsProps"
+    ></v-hands>
+    <v-board v-bind="boardProps" @click="onClick"></v-board>
+    <v-hands v-bind="possessionProps" @click="click"></v-hands>
   </div>
 </template>
 
 <script lang="ts">
-import { createComponent, ref, computed } from '@vue/composition-api'
-import cloneDeep from 'lodash/cloneDeep'
-import firebase, { firestore } from '~/plugins/firebase.js'
-import VBoard from '~/components/molecules/VBoard.vue'
-import VPossession from '~/components/molecules/VPossession.vue'
-import VGameManager from '~/components/molecules/VGameManager.vue'
 import {
-  generatePieces,
-  useHoldingPieces,
-  usePlacePiece,
-  useMovePiece
-} from '~/compositions/piece'
-import { reshape } from '~/compositions/matrix'
-import { Piece, Element, Player, Game } from '~/types/piece'
+  createComponent,
+  computed,
+  onBeforeMount,
+  watch
+} from '@vue/composition-api'
 
+import VBoard from '~/components/molecules/VBoard.vue'
+import VHands from '~/components/molecules/VHands.vue'
+import VGameManager from '~/components/molecules/VGameManager.vue'
+import { useBoard } from '~/compositions/board'
+import {
+  useHoldingPieces,
+  useSelectingPiece
+} from '~/compositions/holding-piece'
+import { Player } from '~/types/player'
+import { Element, Piece, GameRecord } from '~/types/game-record'
+import { useFirestoreGameRecord } from '~/repositories/game-record'
+import { firestore as f } from '~/store'
+import { getTerritory } from '~/functions/matrix'
+import { isWin } from '~/functions/game'
+import { useRandomStrategy } from '~/compositions/storategy'
 type Props = {
-  game: Game
-  docRef: any
   isYourTurn: boolean
+  nextPlayer: Player
+  enableEnemyHands: boolean
+  enableTimer: boolean
 }
 
 export default createComponent({
   components: {
     VGameManager,
     VBoard,
-    VPossession
+    VHands
   },
 
   props: {
-    game: {
-      type: Object,
-      required: true
-    },
-
-    docRef: {
-      type: Function,
-      required: true
-    },
-
     isYourTurn: {
       type: Boolean,
       required: true
+    },
+
+    nextPlayer: {
+      type: String,
+      required: true
+    },
+
+    enableEnemyHands: {
+      type: Boolean,
+      default: false
+    },
+
+    enableTimer: {
+      type: Boolean,
+      default: false
     }
   },
 
-  setup(props: Props, { root }) {
-    const {
-      placePieceRef,
-      mutate: mutatePlacePiece,
-      clear: clearPlacePiece
-    } = usePlacePiece()
+  setup(props: Props, { root, emit }) {
+    const playroomId = f.getPlayroomId
+    const gameId = f.getGameId
+    const gameRecordId = f.getGameRecordId
+    const player: Player = 'PLAYER1'
 
     const {
-      movePieceRef,
-      mutate: mutateMovePiece,
-      clear: clearMovePiece
-    } = useMovePiece()
+      selectingPieceRef,
+      nextValue,
+      mutateMove,
+      mutatePlace
+    } = useSelectingPiece()
 
-    const { holdingPiecesRef, init, pop } = useHoldingPieces()
-    const player: Player = root.$store.getters['player/name']
+    const { getNextMove } = useRandomStrategy()
 
-    init(props.game.rows, props.game.cols, 3, player)
+    const {
+      gameRecordsRef,
+      holdingPieces: holdingPiecesRef,
+      enemyHands,
+      setGameRecordsOfHoldingPieces,
+      getPoppedPieces,
+      getOpponentHands,
+      getPlayerHands
+    } = useHoldingPieces(player)
 
-    const matrix = computed(() => {
-      return reshape(props.game.history.slice(-1)[0].squares, props.game.rows)
+    const {
+      boardRef,
+      setGameRecordsOfBoard,
+      matrixRef,
+      generateMovedBoard,
+      generatePlacedBoard
+    } = useBoard(3)
+
+    const {
+      gameRecordCollectionReference,
+      addGameRecord,
+      setPlayroomId,
+      setGameId
+    } = useFirestoreGameRecord(playroomId, gameId, gameRecordId)
+
+    onBeforeMount(async () => {
+      const gameRecords = (await root.$bind(
+        'gameRecord',
+        gameRecordCollectionReference.value
+          .limit(1)
+          .orderBy('createdAt', 'desc')
+      )) as GameRecord[]
+
+      setGameRecordsOfHoldingPieces(gameRecords)
+      setGameRecordsOfBoard(gameRecords)
     })
 
-    const selectingPiece = computed(() => {
-      return placePieceRef.value || movePieceRef.value
-    })
+    const update = async (a: string, v: string) => {
+      const { gameRecordCollectionReference } = useFirestoreGameRecord(a, v)
 
-    const nextValue = computed(() => {
-      if (selectingPiece.value === undefined) return
-      return selectingPiece.value.piece.value
-    })
+      const gameRecords = (await root.$bind(
+        'gameRecord',
+        gameRecordCollectionReference.value
+          .limit(1)
+          .orderBy('createdAt', 'desc')
+      )) as GameRecord[]
+
+      setGameRecordsOfHoldingPieces(gameRecords)
+      setGameRecordsOfBoard(gameRecords)
+      setPlayroomId(a)
+      setGameId(v)
+    }
+
+    watch(
+      computed(() => {
+        return props.nextPlayer
+      }),
+      (now, prev) => {
+        if (now === 'PLAYER2' && prev === 'PLAYER1') {
+          setTimeout(() => {
+            a()
+            const player2 = getTerritory({
+              matrix: matrixRef.value,
+              player: 'PLAYER2'
+            })
+
+            const win = isWin(player2)
+
+            if (win) {
+              emit('action', { type: 'SETTLE', winner: 'PLAYER2' })
+            } else {
+              emit('action', { type: 'PEND', nextPlayer: 'PLAYER1' })
+            }
+          }, 3000)
+        }
+        console.log(now, prev)
+      }
+    )
+
+    // init(props.game.rows, props.game.cols, 3, player)
 
     const boardProps = computed(() => {
       return {
-        matrix: matrix.value,
+        matrix: matrixRef.value,
         nextValue: nextValue.value,
-        holding: movePieceRef.value ? movePieceRef.value.piece : undefined
+        holding: selectingPieceRef.value
+          ? selectingPieceRef.value.piece
+          : undefined
       }
     })
 
     const possessionProps = computed(() => {
       return {
         holdingPieces: holdingPiecesRef.value,
-        selecting: placePieceRef.value ? placePieceRef.value.piece : undefined,
-        player
+        selectingPiece: selectingPieceRef.value
+          ? selectingPieceRef.value.piece
+          : undefined
+      }
+    })
+
+    const enemyHandsProps = computed(() => {
+      return {
+        holdingPieces: enemyHands.value,
+        selectingPiece: undefined
       }
     })
 
@@ -103,89 +200,108 @@ export default createComponent({
       return {
         isYourTurn: props.isYourTurn,
         player,
-        nextPlayer: props.game.nextPlayer
+        nextPlayer: props.nextPlayer
       }
     })
 
+    const a = async () => {
+      const b = getNextMove(enemyHands.value, boardRef.value, matrixRef.value)
+      if (b === undefined) return
+      const gameRecord: GameRecord = {
+        board: b.board.flat(),
+        player1Hands: getPlayerHands('PLAYER1'),
+        player2Hands: b.hands
+      }
+
+      await addGameRecord(gameRecord)
+    }
+
     const click = (piece: Piece) => {
       if (!props.isYourTurn) return
-      if (movePieceRef.value) {
-        clearMovePiece()
-      }
-      mutatePlacePiece(piece)
+      mutatePlace(piece)
     }
 
-    const onClick = (element: Element) => {
+    const onClick = async (element: Element) => {
+      // console.log(element)
+
       if (!props.isYourTurn) return
-      const { row, col, value } = element
+      const { row, col } = element
 
-      if (selectingPiece.value === undefined) return
+      const b = mutateMove(element)
+      if (!b) return
 
-      if (
-        placePieceRef.value !== undefined &&
-        placePieceRef.value.pieceMotion === 'PLACE'
-      ) {
-        if (
-          !!value.length &&
-          value.slice(-1)[0].value >= placePieceRef.value.piece.value
-        )
-          return
+      switch (b[0]) {
+        case 'PLACED': {
+          const place = generatePlacedBoard({ row, col }, b[1])
 
-        const cloneMatrix = cloneDeep(matrix.value)
-        cloneMatrix[row][col].value.push(placePieceRef.value.piece)
+          const gameRecord: GameRecord = {
+            board: place,
+            player1Hands: getPoppedPieces(b[1]),
+            player2Hands: getOpponentHands()
+          }
+          // emit('action', player)
 
-        props.docRef().update({
-          history: firebase.firestore.FieldValue.arrayUnion({
-            squares: cloneMatrix.flat(),
-            turn: 1,
-            player
-          }),
-          nextPlayer: player === 'PLAYER_1' ? 'PLAYER_2' : 'PLAYER_1'
-        })
-
-        pop(placePieceRef.value.piece)
-        clearPlacePiece()
-      } else if (
-        movePieceRef.value !== undefined &&
-        movePieceRef.value.from !== undefined &&
-        movePieceRef.value.pieceMotion === 'MOVE'
-      ) {
-        const { piece, from } = movePieceRef.value
-        if (!value.length || value.slice(-1)[0].value < piece.value) {
-          const cloneMatrix = cloneDeep(matrix.value)
-          cloneMatrix[row][col].value.push(movePieceRef.value.piece)
-          const { row: frow, col: fcol } = from
-          cloneMatrix[frow][fcol].value.pop()
-
-          props.docRef().update({
-            history: firebase.firestore.FieldValue.arrayUnion({
-              squares: cloneMatrix.flat(),
-              turn: 1,
-              player
-            }),
-            nextPlayer: player === 'PLAYER_1' ? 'PLAYER_2' : 'PLAYER_1'
-          })
+          await addGameRecord(gameRecord)
+          break
         }
-        // else {
-        // }
+        case 'MOVED': {
+          const place = generateMovedBoard(b[2], { row, col }, b[1])
 
-        clearMovePiece()
+          const gameRecord: GameRecord = {
+            board: place,
+            player1Hands: getPlayerHands('PLAYER1'),
+            player2Hands: getPlayerHands('PLAYER2')
+          }
+
+          await addGameRecord(gameRecord)
+          break
+        }
+      }
+
+      const player1T = getTerritory({
+        matrix: matrixRef.value,
+        player: 'PLAYER1'
+      })
+
+      const win = isWin(player1T)
+
+      if (win) {
+        emit('action', { type: 'SETTLE', winner: 'PLAYER1' })
       } else {
-        if (value.slice(-1)[0].player !== player) return
-        mutateMovePiece(element)
+        emit('action', { type: 'PEND', nextPlayer: 'PLAYER1' })
       }
     }
+
+    const defeat = () => {}
+
+    // const defeat = () => {
+    //   props.docRef.update({
+    //     winner: (player === 'PLAYER1' ? 'PLAYER2' : 'PLAYER1') as Player
+    //   })
+
+    //   firestore
+    //     .collection('playrooms')
+    //     .doc(root.$store.getters['playroom/id'])
+    //     .collection('games')
+    //     .add(generateInit(3, 3))
+    // }
 
     return {
-      init,
+      gameRecordsRef,
+      update,
+      a,
+      defeat,
       click,
       onClick,
+      matrixRef,
       boardProps,
       possessionProps,
-      gameManagerProps
+      gameManagerProps,
+      nextValue,
+      holdingPiecesRef,
+      selectingPieceRef,
+      enemyHandsProps
     }
   }
 })
 </script>
-
-<style></style>
