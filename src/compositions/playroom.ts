7572100a4497ae25eb56fd9aa697d83c4ce6e745
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-02-13 22:39:30 +0900

    :sparkles: (playroom.ts) Add compositions of playroom reactive manager

---
+++ src/compositions/playroom.ts
@@ -0,0 +1,35 @@
+import { Ref, computed } from '@vue/composition-api'
+import { Playroom } from '~/types/playroom'
+import { Player } from '~/types/player'
+import { usePlayer } from '~/compositions/player'
+export const usePlayroom = (
+  playroom: Ref<Playroom | undefined>,
+  player: Player
+) => {
+  const { youRef, enemyRef } = usePlayer(player)
+
+  const yourWinsRef = computed((): Playroom['player1Wins'] => {
+    if (!playroom.value) return 0
+    return playroom.value[
+      `${youRef.value}Wins` as 'player1Wins' | 'player2Wins'
+    ]
+  })
+
+  const enemyWinsRef = computed((): Playroom['player2Wins'] => {
+    if (!playroom.value) return 0
+    return playroom.value[
+      `${enemyRef.value}Wins` as 'player1Wins' | 'player2Wins'
+    ]
+  })
+
+  const roundRef = computed((): Playroom['round'] => {
+    if (!playroom.value) return 1
+    return playroom.value.round
+  })
+
+  return {
+    yourWinsRef,
+    enemyWinsRef,
+    roundRef
+  }
+}
