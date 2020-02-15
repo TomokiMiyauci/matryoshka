7af7e166d4d81f8c8de0b4d363bc23ced5fc8636
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-02-15 11:44:14 +0900

    :recycle: (VDoll.stories.ts) Remove other than default description

--- stories/atoms/VDoll.stories.ts
+++ stories/atoms/VDoll.stories.ts
@@ -1,5 +1,6 @@
 import { withInfo } from 'storybook-addon-vue-info'
 import { storiesOf } from '@storybook/vue'
+import { withKnobs, number, radios } from '@storybook/addon-knobs'
 import VDoll from '~/components/atoms/VDoll.vue'
 
 // export default {
@@ -11,50 +12,32 @@ import VDoll from '~/components/atoms/VDoll.vue'
 //   components: { VDoll },
 //   template: '<v-doll color="red"></v-doll>'
 // })
+const options = {
+  red: 'red',
+  blue: 'blue'
+}
 
 storiesOf('Atoms|VDoll', module)
   .addDecorator(withInfo)
+  .addDecorator(withKnobs)
   .add(
     'default',
     () => ({
       components: { VDoll },
-      template: '<v-doll />'
+      template: '<v-doll :color="color" :size="size"></v-doll>',
+      props: {
+        color: {
+          default: radios('color', options, 'red')
+        },
+
+        size: {
+          default: number('size', 30)
+        }
+      }
     }),
     {
       info: {
         summary: 'Atoms of VDoll is image wrapper component.'
-      }
-    }
-  )
-  .add(
-    'color',
-    () => ({
-      components: { VDoll },
-      template: `<div>
-        <v-doll color="red"></v-doll>
-        <v-doll color="blue"></v-doll>
-        </div>`
-    }),
-    {
-      info: {
-        summary: 'Props of color which can pick red of blue.'
-      }
-    }
-  )
-  .add(
-    'size',
-    () => ({
-      components: { VDoll },
-      template: `<div>
-      <v-doll size="30px" color="red"></v-doll>
-      <v-doll size="60px" color="red"></v-doll>
-      <v-doll size="90px" color="red"></v-doll>
-      <div  style="background:grey;width:200px;height:200px;display:flex;justify-content:center;align-items:center;"><v-doll size="50%" color="red"></v-doll></div>
-      </div>`
-    }),
-    {
-      info: {
-        summary: 'Props of size what define image size.'
       }
     }
   )
