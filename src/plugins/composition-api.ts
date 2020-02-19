0a16e56621e567939d0716433454f6c21c5609a1
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-02-20 01:01:37 +0900

    :label: (composition-api.ts) Add interface of SetupContext

--- src/plugins/composition-api.ts
+++ src/plugins/composition-api.ts
@@ -2,3 +2,9 @@ import Vue from 'vue'
 import VueCompositionApi from '@vue/composition-api'
 
 Vue.use(VueCompositionApi)
+
+declare module '@vue/composition-api/dist/component/component' {
+  interface SetupContext {
+    readonly refs: { [key: string]: Vue | Element | Vue[] | Element[] }
+  }
+}
