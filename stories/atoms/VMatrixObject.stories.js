3c00a65daf2ef96ca2e43cc17928e08f5006d393
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-02-20 01:16:26 +0900

    :fire: (VMatrixObject.stories.js) Rename extention to .js

--- stories/atoms/VMatrixObject.stories.js
+++ stories/atoms/VMatrixObject.stories.js
@@ -1,10 +1,15 @@
 import VMatrixObject from '~/components/atoms/VMatrixObject.vue'
 
 export default {
-  title: `atoms|VMatrixObject`
+  title: `atoms|VMatrixObject`,
+  parameters: {
+    info: {
+      summary: 'Atoms of VMatrixObject is for landing page object.'
+    }
+  }
 }
 
-export const def = () => ({
+export const Default = () => ({
   components: { VMatrixObject },
   template: '<v-matrix-object></v-matrix-object>'
 })
